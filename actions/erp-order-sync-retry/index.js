1faf69ffb76cccdd87bbbebc721a575d9314f1b5
Suman Kar <skar@Sumans-MacBook-Pro.local>
2024-04-10 10:47:44 +0530

    changes

--- actions/erp-order-sync-retry/index.js
+++ actions/erp-order-sync-retry/index.js
@@ -1,25 +1,9 @@
 /*
 * <license header>
 */
-
-/**
- * This is a sample action showcasing how to create a cloud event and publish to I/O Events
- *
- * Note:
- * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
- *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
- *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
- *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
- *   - Make sure to validate these changes against your security requirements before deploying the action
- */
-
-
-const { Core, Events } = require('@adobe/aio-sdk')
-const uuid = require('uuid')
-const {
-  CloudEvent
-} = require("cloudevents");
-const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+const fetch = require('node-fetch')
+const { Core } = require('@adobe/aio-sdk')
+var request = require('request');
 
 // main function that will be executed by Adobe I/O Runtime
 async function main (params) {
@@ -27,48 +11,7 @@ async function main (params) {
   const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
 
   try {
-    // 'info' is the default level if not set
-    logger.info('Calling the main action')
-
-    // log parameters, only if params.LOG_LEVEL === 'debug'
-    logger.debug(stringParameters(params))
-
-    // check for missing request input parameters and headers
-    const requiredParams = ['apiKey', 'providerId', 'eventCode', 'payload']
-    const requiredHeaders = ['Authorization', 'x-gw-ims-org-id']
-    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
-    if (errorMessage) {
-      // return and log client errors
-      return errorResponse(400, errorMessage, logger)
-    }
-
-    // extract the user Bearer token from the Authorization header
-    const token = getBearerToken(params)
-
-    
-    // initialize the client
-    const orgId = params.__ow_headers['x-gw-ims-org-id']
-    const eventsClient = await Events.init(orgId, params.apiKey, token)
-
-    // Create cloud event for the given payload
-    const cloudEvent = createCloudEvent(params.providerId, params.eventCode, params.payload)
-
-    // Publish to I/O Events
-    const published = await eventsClient.publishEvent(cloudEvent)
-    let statusCode = 200
-    if (published === 'OK') {
-      logger.info('Published successfully to I/O Events')
-    } else if (published === undefined) {
-      logger.info('Published to I/O Events but there were not interested registrations')
-      statusCode = 204
-    }
-    const response = {
-      statusCode: statusCode,
-    }
-
-    // log the response status code
-    logger.info(`${response.statusCode}: successful request`)
-    return response
+    syncCommerceOrdersToERP();
   } catch (error) {
     // log any server errors
     logger.error(error)
@@ -77,14 +20,65 @@ async function main (params) {
   }
 }
 
-function createCloudEvent(providerId, eventCode, payload) {
-  let cloudevent = new CloudEvent({
-    source: 'urn:uuid:' + providerId,
-    type: eventCode,
-    datacontenttype: "application/json",
-    data: payload,
-    id: uuid.v4()
+/**
+ * Synchronization Order to ERP
+ */
+function syncCommerceOrdersToERP() {
+  var options = {
+    'method': 'GET',
+    'url': 'https://dd45-2401-4900-1cba-a19b-9103-af55-4e08-8c4d.ngrok-free.app/rest/all/V1/orders?'+
+      'searchCriteria[filter_groups][0][filters][0][field]=status&'+
+      '\nsearchCriteria[filter_groups][0][filters][0][condition_type]=eq'+
+      '&searchCriteria[filter_groups][0][filters][0][value]=processing',
+    'headers': {
+      'Authorization': 'Bearer aeazv3wei3qm3g6fi3vvwq7rprwlhh39'
+    }
+  };
+  request(options, function (error, response) {
+    if (error)  {
+      throw new Error(error);
+    } else {
+      var commerceResponse = JSON.parse(response.body);
+      var commerceOrders = commerceResponse.items;
+      /**
+       * Iterate Each Commerce Order,Send Each Order to ERP
+       */
+      commerceOrders.forEach(commerceOrder => {
+        //sendNotificationToSlack(commerceOrder);
+      });
+    }
   });
-  return cloudevent
 }
+
+/**
+ * Update to Slack Channel
+ */
+function sendNotificationToSlack(erpResponse) {
+
+  var slackChannel = "poc";
+  var slackMessage = "*ERP Order Synchronization Details!* \n" + '`' + JSON.stringify(erpResponse) + '`';
+
+  var payload = {
+    "channel": slackChannel,
+    "username": "incoming-webhook",
+    "text": slackMessage,
+    "mrkdwn": true,
+  };
+
+  var options = {
+    method: 'POST',
+    url: "https://hooks.slack.com/services/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",
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(payload)
+  };
+
+  request(options, function (error, response, body) {
+    if (error) {
+      console.log("ERROR: fail to post");
+    } else {
+      console.log ("SUCCESS: posted to slack");
+    }
+  });
+}
+
 exports.main = main
--- sync-order-sample.js
+++ sync-order-sample.js
@@ -1,71 +1,107 @@
 var http = require('http');
+const request = require("request");
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 
   /**
-   * Call Commerce REST API to get Pending Orders
-   * @type {(function(*, *, *): *)|{}}
+   * Synchronization Order to ERP
    */
-  var request = require('request');
-  var options = {
-    'method': 'GET',
-    'url': 'http://local.commerce.com:8085/rest/all/V1/orders?'+
-      'searchCriteria[filter_groups][0][filters][0][field]=status&'+
-      '\nsearchCriteria[filter_groups][0][filters][0][condition_type]=eq'+
-      '&searchCriteria[filter_groups][0][filters][0][value]=processing',
-    'headers': {
-      'Authorization': 'Bearer aeazv3wei3qm3g6fi3vvwq7rprwlhh39'
-    }
-  };
-  request(options, function (error, response) {
-    if (error)  {
-      throw new Error(error);
-    } else {
-      //console.log(response.body);
-      var commerceResponse = JSON.parse(response.body);
-      //console.log(orders.items);
-      var commerceOrders = commerceResponse.items;
-    }
-    /**
-     * Iterate Each Commerce Order
-     */
-    commerceOrders.forEach(commerceOrder => {
-
+  function syncCommerceOrdersToERP() {
+    var options = {
+      'method': 'GET',
+      'url': 'https://dd45-2401-4900-1cba-a19b-9103-af55-4e08-8c4d.ngrok-free.app/rest/all/V1/orders?'+
+        'searchCriteria[filter_groups][0][filters][0][field]=status&'+
+        '\nsearchCriteria[filter_groups][0][filters][0][condition_type]=eq'+
+        '&searchCriteria[filter_groups][0][filters][0][value]=processing',
+      'headers': {
+        'Authorization': 'Bearer aeazv3wei3qm3g6fi3vvwq7rprwlhh39'
+      }
+    };
+    request(options, function (error, response) {
+      if (error)  {
+        throw new Error(error);
+      } else {
+        var commerceResponse = JSON.parse(response.body);
+        var commerceOrders = commerceResponse.items;
+      }
       /**
-       * Send Each Order to ERP
-       * @type {string}
+       * Iterate Each Commerce Order,Send Each Order to ERP
        */
-      var commerOrderData = JSON.stringify(commerceOrder);
-      var options = {
-        method: 'POST',
-        url: "http://localhost:7777/process-order.php?dsdsd=1144",
-        headers: { 'Content-type': 'application/json' },
-        body: commerOrderData
-      };
-
-      request(options, function (error, response, body) {
-        if (error) {
-          console.log("ERROR: Fail to post to ERP");
-          console.log(response.body);
-        } else {
-          console.log ("SUCCESS: Posted to ERP");
-          console.log(response.body);
-
-          /**
-           * Call Commerce Bulk API to update ERP Order Sync Status
-           */
-
-          /**
-           * Call Slack channel for order update
-           */
-
-        }
+      commerceOrders.forEach(commerceOrder => {
+        sendCommerceOrderToERP(commerceOrder);
       });
-
-      console.log("-----------------------------");
     });
-  });
+  }
+
+  /**
+   * Send Order Data to ERP
+   * @param commerceOrder
+   */
+  function sendCommerceOrderToERP(commerceOrder) {
+    var commerOrderData = JSON.stringify(commerceOrder);
+    var options = {
+      method: 'POST',
+      url: "http://localhost:7777/process-order.php",
+      headers: { 'Content-type': 'application/json' },
+      body: commerOrderData
+    };
+
+    request(options, function (error, response) {
+      var erpResponse = response.body;
+      if (error) {
+        console.log("ERROR: Fail to post to ERP");
+        console.log(erpResponse);
+      } else {
+        console.log ("SUCCESS: Posted to ERP");
+        console.log(erpResponse);
+
+        updateCommerceOrderSyncStatus(erpResponse);
+        sendNotificationToSlack(erpResponse);
+      }
+    });
+  }
+
+  /**
+   * Update ERP Order Sync Status to Commerce
+   */
+  function updateCommerceOrderSyncStatus(erpResponse) {
+
+  }
+
+  /**
+   * Update to Slack Channel
+   */
+  function sendNotificationToSlack(erpResponse) {
+
+    var slackChannel = "poc";
+    var slackMessage = "*ERP Order Synchronization Details!* \n" + '`' + erpResponse + '`';
+
+    var payload = {
+      "channel": slackChannel,
+      "username": "incoming-webhook",
+      "text": slackMessage,
+      "mrkdwn": true,
+    };
+
+    var options = {
+      method: 'POST',
+      url: "https://hooks.slack.com/services/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",
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(payload)
+    };
+
+    request(options, function (error, response, body) {
+      if (error) {
+        console.log("ERROR: fail to post");
+      } else {
+        console.log ("SUCCESS: posted to slack");
+      }
+    });
+  }
+
+  syncCommerceOrdersToERP();
 
 }).listen(1337, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
