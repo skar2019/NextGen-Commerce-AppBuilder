afe47e780af23ab3b675b4e63a0159bb4c461b39
Shantanu <cod40403@adobe.com>
2024-04-12 12:32:14 +0530

    ERP single order sync code

--- actions/erp-order-sync/index.js
+++ actions/erp-order-sync/index.js
@@ -7,41 +7,183 @@ var request = require('request');
 
 // main function that will be executed by Adobe I/O Runtime
 async function main (params) {
-  // create a Logger
-  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+    // create a Logger
+    const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+    try {
+        syncCommerceOrderToERP(params);
+    } catch (error) {
+        // log any server errors
+        logger.error(error)
+        // return with 500
+        return errorResponse(500, 'server error', logger)
+    }
+}
 
-  try {
-      var slackChannel = "poc";
-      var slackMessage = "Order Data! \n" + JSON.stringify(params)
+/**
+ * Synchronization Order to ERP
+ */
+function syncCommerceOrderToERP(params) {
+    let order = params.data.value;
+    console.log(order);
+    sendNotificationToSlackGeneric('Order Data Received from Commerce. #' + order.increment_id, params);
+    /**
+     * Send Order to ERP
+     */
+    if (checkERPSyncStatus(order, params)) {
+        sendNotificationToSlackGeneric('Sending order data to ERP. #' + order.increment_id , params);
+        sendCommerceOrderToERP(order, params)
+    }
+}
 
-      var payload = {
-          "channel": slackChannel,
-          "username": "incoming-webhook",
-          "text": slackMessage,
-          "mrkdwn": true,
-      };
+function checkERPSyncStatus(commerceOrder, params){
+    return true;
+}
 
-      var options = {
-          method: 'POST',
-          url: "https://hooks.slack.com/services/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",
-          headers: { 'Content-type': 'application/json' },
-          body: JSON.stringify(payload)
-      };
+/**
+ * Send Order Data to ERP
+ * @param commerceOrder
+ * @param params
+ */
+function sendCommerceOrderToERP(commerceOrder, params) {
+    let commerceOrderData = JSON.stringify(commerceOrder);
+    let options = {
+        method: 'POST',
+        url: params.ERP_API_ENDPOINT + '/process-order.php',
+        headers: { 'Content-type': 'application/json' },
+        body: commerceOrderData
+    };
 
-      request(options, function (error, response, body) {
-          if (error) {
-              console.log("ERROR: fail to post " + response);
-          } else {
-              console.log ("SUCCESS: posted to slack " + response);
-          }
-      });
+    request(options, function (error, response) {
+        let erpResponse = response.body;
 
-  } catch (error) {
-    // log any server errors
-    logger.error(error)
-    // return with 500
-    return errorResponse(500, 'server error', logger)
-  }
+        if (error) {
+            sendNotificationToSlackGeneric('Not able to send order to ERP', params);
+            console.log("ERROR: Fail to post to ERP");
+            console.log(erpResponse);
+        } else {
+            var erpResponseJson = JSON.parse(erpResponse);
+
+            sendNotificationToSlackGeneric('Order Data send to ERP', params);
+            console.log ("SUCCESS: Posted to ERP");
+            console.log(erpResponseJson);
+
+            updateCommerceOrderSyncStatus(erpResponseJson,params);
+            sendNotificationToSlack(erpResponseJson,params);
+        }
+    });
+}
+
+/**
+ * Update ERP Order Sync Status to Commerce
+ *
+ * @param erpResponseJson
+ * @param params
+ */
+function updateCommerceOrderSyncStatus(erpResponseJson, params) {
+
+    let request = require('request');
+    let options = {
+        'method': 'POST',
+        'url': params.COMMERCE_API_ENDPOINT + '/rest/async/bulk/V1/orders',
+        'headers': {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + params.COMMERCE_BEARAR_TOKEN
+        },
+        body: JSON.stringify([{
+            "entity": {
+                "entity_id": erpResponseJson.commerce_order_id,
+                "extension_attributes": {
+                    "erp_sync_status": erpResponseJson.sync_status,
+                    "erp_order_id" : erpResponseJson.erp_order_id
+                }
+            }
+        }])
+
+    };
+    request(options, function (error, response) {
+        if (error) {
+            sendNotificationToSlackGeneric('Not able to update ERP sync Status data to Commerce', params);
+            throw new Error(error);
+        } else {
+            sendNotificationToSlackGeneric('Able to update ERP sync Status to Commerce', params);
+        }
+        console.log(response.body);
+    });
+
+}
+
+/**
+ * Update to Slack Channel
+ */
+function sendNotificationToSlack(erpResponseJson, params) {
+
+    let slackChannel = params.SLACK_CHANNEL_NAME;
+    let erpSyncMessage= 'Order Sync with ERP is Successful';
+
+    if (erpResponseJson.sync_status === '0') {
+        erpSyncMessage = 'Order Sync with ERP is Failed';
+    }
+
+    let slackMessage = "*ERP Order Synchronization Details !* \n" + erpSyncMessage + '\n' +
+        '`' + JSON.stringify(erpResponseJson) + '`';
+
+    let payload = {
+        "channel": slackChannel,
+        "username": "incoming-webhook",
+        "text": slackMessage,
+        "mrkdwn": true,
+    };
+
+    let options = {
+        method: 'POST',
+        url: params.SLACK_POST_URL,
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(payload)
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log("ERROR: fail to post");
+        } else {
+            console.log ("SUCCESS: posted to slack");
+        }
+    });
+}
+
+/**
+ * Send Notification to Slack Generic
+ *
+ * @param message
+ * @param params
+ */
+function sendNotificationToSlackGeneric(message, params) {
+
+    if (params.ENLABLE_DEBUG == 1) {
+        let slackChannel = params.SLACK_DEBUG_CHANNEL_NAME;
+        let slackMessage = '*' + message + '*';
+
+        let payload = {
+            "channel": slackChannel,
+            "username": "incoming-webhook",
+            "text": slackMessage,
+            "mrkdwn": true,
+        };
+
+        let options = {
+            method: 'POST',
+            url: params.SLACK_DEBUG_POST_URL,
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(payload)
+        };
+
+        request(options, function (error, response, body) {
+            if (error) {
+                console.log("ERROR: fail to post");
+            } else {
+                console.log ("SUCCESS: posted to slack");
+            }
+        });
+    }
 }
 
 exports.main = main
--- commerce-order-sync.js
+++ commerce-order-sync.js
@@ -8,8 +8,10 @@ http.createServer(function (req, res) {
    * Call Commerce REST API to get Pending Orders
    * @type {(function(*, *, *): *)|{}}
    */
-  let commerceOrder = '{"base_currency_code":"INR","base_discount_amount":0,"base_grand_total":1005,"base_discount_tax_compensation_amount":0,"base_shipping_amount":5,"base_shipping_discount_amount":0,"base_shipping_discount_tax_compensation_amnt":0,"base_shipping_incl_tax":5,"base_shipping_tax_amount":0,"base_subtotal":1000,"base_subtotal_incl_tax":1000,"base_tax_amount":0,"base_total_due":1005,"base_to_global_rate":1,"base_to_order_rate":1,"billing_address_id":2,"created_at":"2024-04-05 04:31:51","customer_email":"shaandg@gmail.com","customer_firstname":"Shantanu","customer_group_id":0,"customer_is_guest":1,"customer_lastname":"Dasgupta","customer_note_notify":1,"discount_amount":0,"entity_id":1,"global_currency_code":"INR","grand_total":1005,"discount_tax_compensation_amount":0,"increment_id":"000000001","is_virtual":0,"order_currency_code":"INR","protect_code":"bd6f8d447ab2abc76308ee08a616bb9c","quote_id":1,"remote_ip":"::1","shipping_amount":5,"shipping_description":"Flat Rate - Fixed","shipping_discount_amount":0,"shipping_discount_tax_compensation_amount":0,"shipping_incl_tax":5,"shipping_tax_amount":0,"state":"processing","status":"processing","store_currency_code":"INR","store_id":1,"store_name":"Main Website\\r\\nMain Website Store\\r\\nDefault Store View","store_to_base_rate":0,"store_to_order_rate":0,"subtotal":1000,"subtotal_incl_tax":1000,"tax_amount":0,"total_due":1005,"total_item_count":1,"total_qty_ordered":1,"updated_at":"2024-04-05 05:12:18","weight":10,"items":[{"amount_refunded":0,"base_amount_refunded":0,"base_discount_amount":0,"base_discount_invoiced":0,"base_discount_tax_compensation_amount":0,"base_original_price":1000,"base_price":1000,"base_price_incl_tax":1000,"base_row_invoiced":0,"base_row_total":1000,"base_row_total_incl_tax":1000,"base_tax_amount":0,"base_tax_invoiced":0,"created_at":"2024-04-05 04:31:51","discount_amount":0,"discount_invoiced":0,"discount_percent":0,"free_shipping":0,"discount_tax_compensation_amount":0,"is_qty_decimal":0,"is_virtual":0,"item_id":1,"name":"test product","no_discount":0,"order_id":1,"original_price":1000,"price":1000,"price_incl_tax":1000,"product_id":1,"product_type":"simple","qty_canceled":0,"qty_invoiced":0,"qty_ordered":1,"qty_refunded":0,"qty_returned":0,"qty_shipped":0,"quote_item_id":1,"row_invoiced":0,"row_total":1000,"row_total_incl_tax":1000,"row_weight":10,"sku":"test-product","store_id":1,"tax_amount":0,"tax_invoiced":0,"tax_percent":0,"updated_at":"2024-04-05 04:31:51","weight":10}],"billing_address":{"address_type":"billing","city":"Chhindwara","country_id":"IN","email":"shaandg@gmail.com","entity_id":2,"firstname":"Shantanu","lastname":"Dasgupta","parent_id":1,"postcode":"480001","region":"Madhya Pradesh","region_code":"MP","region_id":588,"street":["test","test"],"telephone":"8349110607"},"payment":{"account_status":null,"additional_information":["Check \\/ Money order"],"amount_ordered":1005,"base_amount_ordered":1005,"base_shipping_amount":5,"cc_exp_year":"0","cc_last4":null,"cc_ss_start_month":"0","cc_ss_start_year":"0","entity_id":1,"method":"checkmo","parent_id":1,"shipping_amount":5},"status_histories":[],"extension_attributes":{"shipping_assignments":[{"shipping":{"address":{"address_type":"shipping","city":"Chhindwara","country_id":"IN","email":"shaandg@gmail.com","entity_id":1,"firstname":"Shantanu","lastname":"Dasgupta","parent_id":1,"postcode":"480001","region":"Madhya Pradesh","region_code":"MP","region_id":588,"street":["test","test"],"telephone":"8349110607"},"method":"flatrate_flatrate","total":{"base_shipping_amount":5,"base_shipping_discount_amount":0,"base_shipping_discount_tax_compensation_amnt":0,"base_shipping_incl_tax":5,"base_shipping_tax_amount":0,"shipping_amount":5,"shipping_discount_amount":0,"shipping_discount_tax_compensation_amount":0,"shipping_incl_tax":5,"shipping_tax_amount":0}},"items":[{"amount_refunded":0,"base_amount_refunded":0,"base_discount_amount":0,"base_discount_invoiced":0,"base_discount_tax_compensation_amount":0,"base_original_price":1000,"base_price":1000,"base_price_incl_tax":1000,"base_row_invoiced":0,"base_row_total":1000,"base_row_total_incl_tax":1000,"base_tax_amount":0,"base_tax_invoiced":0,"created_at":"2024-04-05 04:31:51","discount_amount":0,"discount_invoiced":0,"discount_percent":0,"free_shipping":0,"discount_tax_compensation_amount":0,"is_qty_decimal":0,"is_virtual":0,"item_id":1,"name":"test product","no_discount":0,"order_id":1,"original_price":1000,"price":1000,"price_incl_tax":1000,"product_id":1,"product_type":"simple","qty_canceled":0,"qty_invoiced":0,"qty_ordered":1,"qty_refunded":0,"qty_returned":0,"qty_shipped":0,"quote_item_id":1,"row_invoiced":0,"row_total":1000,"row_total_incl_tax":1000,"row_weight":10,"sku":"test-product","store_id":1,"tax_amount":0,"tax_invoiced":0,"tax_percent":0,"updated_at":"2024-04-05 04:31:51","weight":10}]}],"payment_additional_info":[{"key":"method_title","value":"Check \\/ Money order"}],"applied_taxes":[],"item_applied_taxes":[],"gift_cards":[],"base_gift_cards_amount":0,"gift_cards_amount":0,"gw_base_price":"0.0000","gw_price":"0.0000","gw_items_base_price":"0.0000","gw_items_price":"0.0000","gw_card_base_price":"0.0000","gw_card_price":"0.0000"}}';
+  let commerceOrder = '{"source":"urn:uuid:aa89ea71-b52b-4f44-866a-2a2d962fe701","data":{"_metadata":{"commerceEdition":"Adobe Commerce","commerceVersion":"2.4.6-p4","eventsClientVersion":"1.6.0","storeGroupId":"1","storeId":"1","websiteId":"1"},"key":"ddaa98ca-bd8b-4fb9-9f01-8035ec430e62","source":"NextGenCommerce.stage","value":{"created_at":"2024-04-05 13:16:02","customer_id":"5","discount_amount":0,"entity_id":"7","grand_total":53,"increment_id":"000000007","items":[{"discount_amount":0,"name":"Laser Cut Stretch Belt","price":48,"qty_ordered":1,"row_total":48,"row_total_incl_tax":48,"sku":"VA07-BC-S","tax_amount":0},{"discount_amount":0,"name":"Laser Cut Stretch Belt","price":0,"qty_ordered":1,"row_total":"0.0000","sku":"VA07-BC-S","tax_amount":"0.0000"}],"order_currency_code":"USD","payment":{"method":"checkmo"},"shipping_description":"Flat Rate - Fixed","state":"new","status":"pending","subtotal":48}},"id":"aa41052d-5c42-4437-84cc-79cc101efc7b","recipient_client_id":"259721d002e04ebc806c28da8696975f","time":"2024-04-05T13:16:04.553Z","type":"com.adobe.commerce.observer.sales_order_save_commit_after","LOG_LEVEL":"debug","datacontenttype":"application/json","event_id":"0e5f7a04-10d8-43b0-b7ac-8c0cea9e3d46","specversion":"1.0"}';
   console.log(commerceOrder)
+  commerceOrder = JSON.parse(commerceOrder);
+  let order = commerceOrder.data.value;
   let erpSyncOptions = {
     method: 'POST',
     url: "http://localhost:7777/process-order.php?dsdsd=1144",
@@ -17,27 +19,32 @@ http.createServer(function (req, res) {
       'Content-type': 'application/json',
       'Authorization': 'Bearer eyJraWQiOiIxIiwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOjEsInV0eXBpZCI6MiwiaWF0IjoxNzEyNTU4MjkzLCJleHAiOjE3MTI1NjE4OTN9.9HnQn6TPcKJpYtg0kL2PTph8IWYm_YphLSFzr4zKTD8'
     },
-    body: commerceOrder
+    body: order
   };
-  commerceOrder = JSON.parse(commerceOrder);
+
 
   let slackText = "";
   request(erpSyncOptions, function (error, response, body) {
     if (error) {
-      console.log("ERROR: Fail to post to ERP");
-      console.log(response.body);
-      slackText = "Order failed to sync to ERP. Order ID: " + commerceOrder.increment_id;
+      console.log("ERROR: Fail to post to ERP. #" + order.increment_id);
+      console.log(body);
+      slackText = "Order failed to sync to ERP. Order ID: " + order.increment_id;
     } else {
-      slackText = "Order successfully synced to ERP. Order ID: " + commerceOrder.increment_id;
-      console.log ("SUCCESS: Posted to ERP");
-      console.log(response.body);
+      if (body.sync_status == 1) {
+        slackText = "Order successfully synced to ERP. Order ID: " + order.increment_id;
+        console.log ("SUCCESS: Posted to ERP. #" + order.increment_id);
+      } else {
+        slackText = "Order failed to sync to ERP. Order ID: " + order.increment_id;
+        console.log("ERROR: Order Sync failed! #" + order.increment_id);
+      }
+      console.log(body);
 
       /**
        * Call Commerce Bulk API to update ERP Order Sync Status
        */
       let commerceOrderUpdateBody = [{
         "entity": {
-          "entity_id": commerceOrder.entity_id,
+          "entity_id": order.entity_id,
           "state": "processing",
           "status": "processing"
         }
@@ -52,9 +59,9 @@ http.createServer(function (req, res) {
 
       request(commerceOption, function (error, response, body) {
         if (error) {
-          console.log("ERP status update in commerce failed.!! " + response.body);
+          console.log("ERP status update in commerce failed.!! " + body);
         } else {
-          console.log("ERP status update in commerce success. " +  response.body);
+          console.log("ERP status update in commerce success. " +  body);
         }
       });
 
